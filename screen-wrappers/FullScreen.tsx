2ddbced0ad37bce42bee787e1ef3df6cfe260f2a
ederzin13 <edermurilo131313@gmail.com>
2026-04-02 20:51:36 -0300

    screen-wrapper fullscreen

---
+++ screen-wrappers/FullScreen.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+type FullScreenProps = {
+  children: React.ReactNode;
+};
+
+export default function FullScreen({ children }: FullScreenProps) {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
